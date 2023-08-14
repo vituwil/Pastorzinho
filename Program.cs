d59130c9296461ebbac2d90d5fcdee380f1aa503
Victor <victorrefundine@hotmail.com>
2023-08-14 19:25:37 -0300

    Adição da canção O Pastorzinho

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.Write("Dó, "); Console.Beep(1320, 200);
+Console.Write("ré, "); Console.Beep(1485, 200);
+Console.Write("mi, "); Console.Beep(1650, 200);
+Console.Write("fá, "); Console.Beep(1759, 200);
+Thread.Sleep(500);
+Console.Write("fá, "); Console.Beep(1759, 200);
+Console.Write("fá,"); Console.Beep(1759, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Red;
+
+Console.Write("Dó, "); Console.Beep(1320, 200);
+Console.Write("ré, "); Console.Beep(1485, 200);
+Console.Write("dó, "); Console.Beep(1320, 200);
+Console.Write("ré, "); Console.Beep(1485, 200);
+Thread.Sleep(500);
+Console.Write("ré, "); Console.Beep(1485, 200);
+Console.Write("ré,"); Console.Beep(1485, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+
+Console.Write("Dó, "); Console.Beep(1320, 200);
+Console.Write("sol, "); Console.Beep(1980, 200);
+Console.Write("fá, "); Console.Beep(1759, 200);
+Console.Write("mi, "); Console.Beep(1650, 200);
+Thread.Sleep(500);
+Console.Write("mi, "); Console.Beep(1650, 200);
+Console.Write("mi,"); Console.Beep(1650, 200);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+Console.Write("Dó, "); Console.Beep(1320, 200);
+Console.Write("ré, "); Console.Beep(1485, 200);
+Console.Write("mi, "); Console.Beep(1650, 200);
+Console.Write("fá, "); Console.Beep(1759, 200);
+Thread.Sleep(500);
+Console.Write("fá, "); Console.Beep(1759, 200);
+Console.Write("fá."); Console.Beep(1759, 200);
+Thread.Sleep(1000);
+
+Console.ResetColor();
